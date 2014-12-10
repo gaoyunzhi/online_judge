ee15acb1225e4a692c2523fb120ba7c35705f4f1
Yunzhi Gao <gyz@fb.com>
2014-12-09 18:54:19 -0800

    second

--- vijos1039.py
+++ vijos1039.py
@@ -7,35 +7,37 @@ class Solver():
 
   def solve(self, occurrence, length):
     if length % 2 == 1:
-      return abs(*self.parts(occurence, length))
+      return abs(*self.parts(occurrence, length))
 
   def solveRem(self, numbers):
     occurrence = [0] * 10
     for number in numbers:
       occurrence[number] += 1
-    occurrence = tuple(occurrence)
+    occurrence_string = []
+    for i in xrange(10):
+      occurrence_string.append(str(i) * occurrence)
     if not occurrence in self._cache:
       self._cache[occurrence] = self.solve(occurrence, len(numbers))
     return self._cache[occurrence]
  
 
-  def parts(self, occurence, length):
+  def parts(self, occurrence, length):
     if length == 0:
       return 0, 0
     numbers = list(occurrence)
     asc_part = 0
     if length % 2 == 1:
-      min_num = self.findMin(occurence)
+      min_num = self.findMin(occurrence)
       numbers[min_num] -= 1
       asc_part = min_num
     cur_num = 0
     for _ in xrange(length / 2):
-      if occurence[cur_num]
+      if occurrence[cur_num]
 
 
-  def findMin(self, occurence):
+  def findMin(self, occurrence):
     for i in xrange(1, 10):
-      if occurence[i] > 0:
+      if occurrence[i] > 0:
         return i
 
 def main():
