4accddf10113f32fb9c5aa53f4017562fde9d01d
Yunzhi Gao <gyz@fb.com>
2014-12-09 23:17:02 -0800

    太感人了

--- vijos1002.py
+++ vijos1002.py
@@ -5,18 +5,22 @@ def main():
   handle = open("1002.txt", "r") 
   length_bridge = int(handle.readline())
   min_step, max_step, num_stone = map(int, handle.readline().split())
-  stone_locations = map(int, handle.readline().split())[:num_stone]
+  stone_locations = map(int, handle.readline().split())
   stone_locations.sort()
   stone_locations.append(length_bridge)
-  for index in xrange(len(stone_locations) - 1):
+  stone_locations = [0] + stone_locations
+  new_stone_locations = [0]
+  shorten = 0
+  for index in xrange(num_stone + 1):
     if stone_locations[index + 1] - stone_locations[index] > min_step * max_step:
-      shorten = (stone_locations[index + 1] - stone_locations[index] - \
-        min_step * max_step) / max_step
-      stone_locations[index + 1] -= shorten * max_step
-  length_bridge = stone_locations[-1]
-  del stone_locations[-1]
+      shorten += ((stone_locations[index + 1] - stone_locations[index] - \
+        min_step * max_step) / max_step)
+    new_stone_locations.append(stone_locations[index + 1] - shorten * max_step)
+  length_bridge = new_stone_locations[-1]
+  del new_stone_locations[-1]
+  del new_stone_locations[0]
   ans = [0]
-  stone_locations = set(stone_locations)
+  stone_locations = set(new_stone_locations)
   for i in xrange(1, length_bridge + max_step * min_step + 1):
     ans.append(min( \
       ans[max(0, i - max_step): max(-1, i - min_step) + 1] + [length_bridge] \
