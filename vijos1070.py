0ccf70e8e30f562fbe5c1f2a6d0c51647189471d
Yunzhi Gao <gyz@fb.com>
2014-12-10 14:30:46 -0800

    --

--- vijos1070.py
+++ vijos1070.py
@@ -5,12 +5,10 @@ class Node():
   def __init__(self, key):
     self.key = key
     self.root = self
-    self.mstParent = self
     self.rank = 0
 
   def setParent(self, new_parent):
     self.root = new_parent
-    self.mstParent = new_parent
     self.rank = new_parent.rank + 1
 
   def findRoot(self):
@@ -19,65 +17,58 @@ class Node():
     self.root = self.root.findRoot()
     return self.root
 
-  def getMstParent(self):
-    return self.mstParent
-
   def getRank(self):
     return self.rank
-
-  def clearRoot(self):
-    self.root = self.mstParent
-
-  def setMstParent(self, new_mst_parent):
-    self.mstParent = new_mst_parent
 
 class Solve():
   def __init__(self, n, edges):
     self.edges = edges
     self.nodes = [Node(x) for x in xrange(n)]
     self.mstEdgeInd = set()
-    self.calulateMst()
+    self.calulateMST()
     self.calulateSecondMst()
 
-  def calulateMst(self):
+  def calulateMST(self):
     self.mstTotal = 0
+    self.mst_node_map = [[]] * len(self.nodes)
     for index, (i, j, cost) in enumerate(self.edges):
-      rootA = self.nodes[i].findRoot()
-      rootB = self.nodes[j].findRoot()
+      rootA, rootB = self.nodes[i].findRoot(), self.nodes[j].findRoot()
       if rootA == rootB:
         continue
       self.mstEdgeInd.add(index)
       self.mstTotal += cost
       if rootB.getRank() > rootA.getRank():
-        rootA, rootB = rootB, rootA
+        rootA, rootB, = rootB, rootA
       rootA.setParent(rootB)
+      self.mst_node_map[i].append(j)
+      self.mst_node_map[j].append(i)
     roots = map(lambda node: node.findRoot(), self.nodes)
     if not (roots == [roots[0]] * len(roots)):
       self.mstTotal = -1
+      return
+
+  def findPartition(self, u, v):
+    self.partition.add(u)
+    for new_node in self.mst_node_map[u]:
+      if new_node == v or new_node in self.partition:
+        continue
+      self.findPartition(new_node, v)
 
   def calulateSecondMst(self):
     if self.mstTotal == -1:
       self.secondMSTCost = -1
       return
     self.secondMSTCost = float('Inf')
-    for index in self.mstEdgeInd:
-      u, v, cost_uv = self.edges[index]
-      nodeA = self.nodes[u]
-      nodeB = self.nodes[v]
-      if nodeA.getMstParent() == nodeB:
-        nodeA, nodeB = nodeB, nodeA
-      nodeB.setMstParent(nodeB)
-      map(lambda node: node.clearRoot(), self.nodes)
-      for index, (i, j, cost_ij) in enumerate(self.edges):
-        if index in self.mstEdgeInd: continue
-        rootI = self.nodes[i].findRoot()
-        rootJ = self.nodes[j].findRoot()
-        if rootI == rootJ: 
-          continue
-        self.secondMSTCost = \
-          min(self.secondMSTCost, self.mstTotal - cost_uv + cost_ij)
-        break
-      nodeB.setMstParent(nodeA)
+    for index_uv in self.mstEdgeInd:
+      u, v, cost_uv = self.edges[index_uv]
+      self.partition = set()
+      self.findPartition(u, v)
+      for index_ij, (i, j, cost_ij) in enumerate(self.edges):
+        if index_ij in self.mstEdgeInd: continue
+        if (i in self.partition) ^ (j in self.partition): 
+          self.secondMSTCost = \
+            min(self.secondMSTCost, self.mstTotal - cost_uv + cost_ij)
+          break
     if self.secondMSTCost == float('Inf'):
       self.secondMSTCost = -1
 
@@ -96,3 +87,15 @@ def main():
   
 if __name__ == '__main__':
   main()
+
+'''
+tests:
+4 6
+1 2 2
+2 3 2
+3 4 4
+4 1 2
+1 3 1
+2 4 4
+
+'''
