4c22abb679be9615dee42d1ede9d54c073fe1596
Yunzhi Gao <gyz@fb.com>
2014-12-10 11:49:55 -0800

    书山有路勤为径

--- vijos1005.py
+++ vijos1005.py
@@ -1,4 +1,5 @@
 # https://vijos.org/p/1039
+# test cases http://acm.timus.ru/forum/?space=1&num=1165
 import sys, random
 
 class Solver():
@@ -55,19 +56,16 @@ class Solver():
       if second_part_int > self.min:
         continue
       for num in xrange(1, start_ind + 1):
-        if second_part_int > 1:
-          potential_string_1 = str(second_part_int - 1) + '9' * num
-        else:
-          potential_string_1 = '9' * num
-        if potential_string_1 != '0' and potential_string_1.endswith(first_part):
-          self._updateMin(int(potential_string_1) + 1, start_ind)
-        potential_string_2 = str(second_part_int) + first_part[start_ind - num:]
-        if potential_string_2.endswith(first_part):
-          self._updateMin(int(potential_string_2) + 1, start_ind)
+        tail = first_part[start_ind - num:]
+        if tail == '9' * num and second_part_int > 1 \
+          and str(second_part_int - 1).endswith(first_part[:start_ind - num]):
+          self._updateMin(int(str(second_part_int - 1) + tail) + 1, start_ind)
+        if tail != '9' * num and second_part.endswith(first_part[:start_ind - num]):
+          self._updateMin(int(second_part + tail) + 1, start_ind)
 
   def _updateMin(self, new_min, pos):
     if new_min == self.min:
-      self.minpos = min(self.minpos, pos)
+      self.minpos = max(self.minpos, pos)
       return
     if new_min < self.min:
       self.min = new_min
@@ -81,33 +79,38 @@ def main():
   sys.stdout.write(str(sol.getAns()))
    
 def test():
+  print Solver('0202').min
+  print Solver('0202').getAns()
+  print Solver('0202').getAns() == 6971
+  print Solver('999').getAns() == 2588
+
   string = ''.join(map(str, xrange(1, 100000)))
-  for i in xrange(len(string)):
-    if random.random() > 0.005: continue
-    for j in xrange(i, len(string)):
-      if random.random() > 0.005: continue
-      substring = string[i: j + 1]
-      ind = string.find(substring)
-      if Solver(substring).getAns() != ind + 1:
-        print '--', i, j
-        print substring, Solver(substring).getAns()
+  # for i in xrange(len(string)):
+  #   if random.random() > 0.005: continue
+  #   for j in xrange(i, len(string)):
+  #     if random.random() > 0.005: continue
+  #     substring = string[i: j + 1]
+  #     ind = string.find(substring)
+  #     if Solver(substring).getAns() != ind + 1:
+  #       print '--', i, j
+  #       print substring, Solver(substring).getAns()
 
 
-  # print Solver(string[:100]).getAns() == 1
-  # print Solver('1').getAns() == 1
-  # print Solver('5').getAns() == 5
-  # print Solver('101').getAns() == 10
-  # print Solver('01').getAns() == 11
-  # print Solver('1011').getAns() == 10
-  # print Solver('12345').getAns() == 1
-  # print Solver('012345').min
-  # #print Solver('012345').getAns()
-  # print Solver('9920').min
-  # #print Solver('9920').getAns()
-  # print Solver('0112').min
-  # #print Solver('0112').getAns()
+  print Solver(string[:100]).getAns() == 1
+  print Solver('1').getAns() == 1
+  print Solver('5').getAns() == 5
+  print Solver('101').getAns() == 10
+  print Solver('01').getAns() == 11
+  print Solver('1011').getAns() == 10
+  print Solver('12345').getAns() == 1
+  print Solver('012345').min
+  #print Solver('012345').getAns()
+  print Solver('9920').min
+  #print Solver('9920').getAns()
+  print Solver('0112').min
+  #print Solver('0112').getAns()
 
 
 if __name__ == '__main__':
-  main()
-  # test()
+  # main()
+  test()
